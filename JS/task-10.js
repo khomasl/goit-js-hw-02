4d106341ec4ca366d4cf05363bb32eef0ae9916a
khomasl <gymnasium.yv@gmail.com>
2021-05-30 22:23:45 +0300

    Задачи 01-10

--- JS/task-10.js
+++ JS/task-10.js
@@ -1,14 +1,8 @@
-// Функция makeMessage(name, price) составляет и возвращает сообщение о покупке. Она объявляет два параметра, значения которых будут задаваться во время её вызова.
-
-// name - название товара
-// price - цена товара
-// Дополни код функции так, чтобы в переменную message записывалась строка "You picked <product name>, price per item is <product price> credits", где <product name> и <product price> это значения параметров name и price. Используй синтаксис шаблонных строк.
-
-// Внимание
-// // Обрати внимание на то, что в коде нет вызовов функции makeMessage. С этого задания и дальше мы сами будем вызывать твои функции и проверять то, как они работают. Результат наших проверок ты увидишь в блоке Результаты под редактором кода.
-function makeMessage (name, price) {
-    // Change code below this line
-     const message = `You picked ${name}, price per item is ${price} credits`;
-    // Change code above this line
-    return message;
-  };
+// Дополни код функции splitMessage(message, delimeter) так, чтобы она возвращала в переменной words результат разделения строки message по разделителю delimeter - массив строк.
+function splitMessage(message, delimeter) {
+  let words;
+  // Change code below this line
+  words = message.split(delimeter);
+  // Change code above this line
+  return words;
+}
